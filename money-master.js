25a69721fccf1ba5b043c11a9462136416ee2888
msomon <hsomon940@gmail.com>
2022-02-17 21:28:01 +0600

    add error massage

--- money-master.js
+++ money-master.js
@@ -1,8 +1,15 @@
 
 function allCost(input){
-    const inputText = document.getElementById(input) ;
-    const inputValue = parseFloat(inputText.value); 
-   return inputValue ;
+  const inputText = document.getElementById(input) ;
+const inputCost = parseFloat(inputText.value);
+if(isNaN(inputCost) || inputCost < 0){
+  inputText.value = ""
+    return alert('please give positive number')
+}else{
+  inputText.value = ""
+  return inputCost ;
+}
+    
 }
 
 document.getElementById('calculate').addEventListener('click',function(){
@@ -11,21 +18,22 @@ const rent = allCost('rent');
 const cloth = allCost('cloth');
 
 const totalExpense = document.getElementById('expense');
-  totalExpense.innerText = food + rent + cloth ;
+totalExpense.innerText = food + rent + cloth ;
   
   const incomeMoney = document.getElementById('income');
   const income = incomeMoney.value ;
+  incomeMoney.value = ""
 
   const balanceMoney = document.getElementById('balance');
  const balance = parseFloat(balanceMoney.innerText);
- balanceMoney.innerText = income - totalExpense.innerText  ;
  
-// if(totalExpense.innerText > income){
-//   const notify = document.getElementById('notify-number');
-//   notify.style.display = 'block'
-// }else{
-//   balanceMoney.innerText = income - totalExpense.innerText  ;
-// }
+ 
+ if(totalExpense.innerText > income){
+   return alert('please decreage your total expense')
+
+ }else{
+  balanceMoney.innerText = income - totalExpense.innerText  ;
+ }
 
 })
 

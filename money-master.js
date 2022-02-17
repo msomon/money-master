99b696d30af766c48ac909c4b7dae4e3d8955d9a
msomon <hsomon940@gmail.com>
2022-02-17 23:36:27 +0600

    update remaining price

--- money-master.js
+++ money-master.js
@@ -2,11 +2,10 @@
 function allCost(input){
   const inputText = document.getElementById(input) ;
 const inputCost = parseFloat(inputText.value);
-if(isNaN(inputCost) || inputCost < 0){
+if( inputCost < 0){
   inputText.value = ""
-    return alert('Please Give Positive Number')
+    return alert('Please Give Positive Number ')
 }else{
-  inputText.value = ""
   return inputCost ;
 }    
 }
@@ -20,23 +19,11 @@ const cloth = allCost('cloth');
 const totalExpense = document.getElementById('expense');
 totalExpense.innerText = food + rent + cloth ;
   
-  // const incomeText = document.getElementById('income');
-  // const incomeMoney = incomeText.value ;
-  // if(isNaN(incomeMoney) || incomeMoney < 0){
-  //   incomeText.value = ""
-  //     return alert('Please Give Positive Number')
-  // }
-  // else{
-  //   incomeText.value = ""
-  //   return incomeMoney ;
-  // } 
-  // const incomeText = document.getElementById('income');   
+  // const incomeMoneyText = document.getElementById('income');
   const incomeMoney = allCost('income')
-  
-
   const balanceText = document.getElementById('balance');
  
- if(totalExpense.innerText > incomeMoney ){
+ if(totalExpense.innerText > incomeMoney.value ){
    return alert('Please Decreage Your Total Expense ')
 
  }else{
@@ -47,21 +34,21 @@ totalExpense.innerText = food + rent + cloth ;
   //   add save  clic handaler  //
 document.getElementById('save-btn').addEventListener('click',function(){
   const save = document.getElementById('save');
-  const incomeText = document.getElementById('income');
+  const incomeText = document.getElementById('income'); 
   const savingAmount = document.getElementById('saving-amount');
     const saveAmount = incomeText.value * (save.value / 100);
     savingAmount.innerText = saveAmount ;
    const balance = document.getElementById('balance');
    const remainingBalance = document.getElementById('remaining-balance');
-
-   remainingBalance.innerText = balance.innerText - savingAmount.innerText ;
+   const totalExpense = document.getElementById('expense');
+ const total = savingAmount.innerText + totalExpense.innerText ;
 
    //     error handling    //
-   const totalExpense = document.getElementById('expense');
-   const total = totalExpense.innerText + savingAmount.innerText
-   if(total >incomeText.value){
-    return alert('Please Decreage Your Total Expense Or Saving')
-   }else{
+   if(total >incomeText.value ){
+     return alert('Please Decreage Your Total Expense Or Saving Money')
+   }
+   else {
     remainingBalance.innerText = balance.innerText - savingAmount.innerText ;
    }
+   
 })

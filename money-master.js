0042e3a03ad5d78f61cc9f8c0f8b0c58dd27f5c9
msomon <hsomon940@gmail.com>
2022-02-17 08:21:09 +0600

    total amount

--- money-master.js
+++ money-master.js
@@ -1,4 +1,7 @@
+
+
 document.getElementById('calculate').addEventListener('click',function(){
+ 
  
   const foodCost = document.getElementById('food') ;
   const rentCost = document.getElementById('rent');
@@ -6,8 +9,27 @@ document.getElementById('calculate').addEventListener('click',function(){
   const food = parseFloat(foodCost.value);
   const rent = parseFloat(rentCost.value);
   const cloth = parseFloat(clothCost.value);
- let total = food + rent + cloth ;
+ const total = food + rent + cloth ;
+ 
+  const totalExpense = document.getElementById('expense');
+  // const expense = parseFloat(totalExpense.innerText);
+  totalExpense.innerText = total  ; 
 
-  let allTotal = document.getElementById('expense');
-   allTotal.innerText = total + allTotal ;
+  const incomeMoney = document.getElementById('income');
+  const balanceMoney = document.getElementById('balance');
+ const income = incomeMoney.value
+ const balance = parseFloat(balanceMoney.innerText);
+ balanceMoney.innerText = income - totalExpense.innerText  ;
+//  balanceMoney.innerText = balanceTotal
+
+})
+document.getElementById('save-btn').addEventListener('click',function(){
+  const save = document.getElementById('save');
+  const balanceMoney = document.getElementById('balance');
+  const savingAmount = document.getElementById('saving-amount');
+    // const saveAmount =  parseFloat(balanceMoney.innerText) / parseFloat(save.value) ;
+    const saveAmount =balanceMoney.innerText / save.value;
+    savingAmount.innerText = saveAmount ;
+  
+
 })

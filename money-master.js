88b6e633284c0cb79a88ab32b200812c718a9500
msomon <hsomon940@gmail.com>
2022-02-21 09:49:02 +0600

    solved some issu

--- money-master.js
+++ money-master.js
@@ -17,13 +17,15 @@ const rent = allCost('rent');
 const cloth = allCost('cloth');
 
 const totalExpense = document.getElementById('expense');
+// const totalExpense = parseFloat(totalExpenseText.innerText)
 totalExpense.innerText = food + rent + cloth ;
+console.log(totalExpense.innerText);
   
   // const incomeMoneyText = document.getElementById('income');
   const incomeMoney = allCost('income')
   const balanceText = document.getElementById('balance');
  
- if(totalExpense.innerText > incomeMoney.value ){
+ if(totalExpense.innerText > incomeMoney){
    return alert('Please Decreage Your Total Expense ')
 
  }else{
@@ -33,14 +35,24 @@ totalExpense.innerText = food + rent + cloth ;
 })
   //   add save  clic handaler  //
 document.getElementById('save-btn').addEventListener('click',function(){
-  const save = document.getElementById('save');
-  const incomeText = document.getElementById('income'); 
+  const saveText = document.getElementById('save');
+ const save = parseFloat(saveText.value);
+  const incomeText = document.getElementById('income');
+  const income = parseFloat(incomeText.value);
   const savingAmount = document.getElementById('saving-amount');
-    const saveAmount = incomeText.value * (save.value / 100);
+    const saveAmount = income * (save / 100);
     savingAmount.innerText = saveAmount ;
+
    const balance = document.getElementById('balance');
    const remainingBalance = document.getElementById('remaining-balance');
-   remainingBalance.innerText = balance.innerText - savingAmount.innerText ;
-   
+   const totalExpense = document.getElementById('expense')
+ const total = parseFloat(totalExpense.innerText) + parseFloat(savingAmount.innerText) ;
+
+ if(total > income ){
+  return alert('Please Decreage Your Total Expense Or Saving Amount ')
+ }
+ else{
+  remainingBalance.innerText = balance.innerText - savingAmount.innerText ;
+ }
    
 })
